b62313e74c6a0a2744b8818498a15b947294d0c7
ri5ka <riskasantoso07@gmail.com>
2024-04-20 20:59:59 +0700

    Riska Anggraeni Santoso

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Aplikasi Flutter',
+            style: TextStyle(color: Colors.black), 
+          ),
+          backgroundColor: Colors.orange, 
+        ),
+        body: Container(
+          color: Colors.pink, 
+          child: Center(
+            child: Text(
+              'Hello Riska Anggraeni Santoso',
+              style: TextStyle(
+                fontSize: 24.0,
+                backgroundColor: Colors.pink, 
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
